edf6b9150d5d145bef1f39bbc89f18582875cc48
Zhazgul <zhli230490@gmail.com>
2024-01-31 19:33:29 +0600

    updated ByClassNameTest- commit

--- src/main/java/com/fall23/locators/ByClassNameTest.java
+++ src/main/java/com/fall23/locators/ByClassNameTest.java
@@ -6,18 +6,17 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-import static com.fall23.WebDriverManager.findElementByClassName;
-import static com.fall23.WebDriverManager.openTheSite;
+import static com.fall23.WebDriverManager.*;
 
 public class ByClassNameTest extends BaseTest {
     @Test
     void byClassNameTest (){
         openTheSite("https://demoqa.com/text-box");
 
-        WebElement usernameInput = driver.findElement(By.id("userName"));
-        WebElement emailInput = driver.findElement(By.id("userEmail"));
-        WebElement currentAddressInput = driver.findElement(By.id("currentAddress"));
-        WebElement permanentAddressInput = driver.findElement(By.id("permanentAddress"));
+        WebElement usernameInput = findElementById("userName");
+        WebElement emailInput = findElementById("userEmail");
+        WebElement currentAddressInput = findElementById("currentAddress");
+        WebElement permanentAddressInput = findElementById("permanentAddress");
         //WebElement submitBtn = driver.findElement(By.className("btn-primary"));
         WebElement submitBtn = findElementByClassName("btn-primary");
 
